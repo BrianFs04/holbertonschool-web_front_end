ffb562d6d06c7c300e40b55979c7928ef568d706
BrianFs04 <brayanflorezsanabria@gmail.com>
2021-02-16 13:01:44 -0500

    [Feature]: Execution stack and timing execution

---
+++ 0x0C-Javascript_advanced/10-prime.js
@@ -0,0 +1,31 @@
+// Execution stack and timing execution
+function countPrimeNumbers() {
+    let count = 0;
+    for (let i = 2; i <= 100; i++) {
+        let prime = true;
+        for (let j = 2; j < i; j++) {
+            if (i % j === 0) {
+                prime = false;
+                break;
+            }
+        }
+        if (prime) {
+            count++;
+        }
+    }
+    return count;
+}
+
+const start = performance.now();
+
+for (let i = 1; i <= 100; i++) {
+    countPrimeNumbers();
+}
+
+const end = performance.now();
+
+const timeUsed = end - start;
+
+console.log(
+    `Execution time of calculating prime numbers 100 times was ${timeUsed} milliseconds.`
+);
